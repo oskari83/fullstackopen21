7a9bbe9491be8d0b52a48a173460286755425635
oskari83 <oskpelto@gmail.com>
2022-05-13 20:14:58 +0300

    part 8 9.1-9.3

---
+++ part8/proj1/exerciseCalculator.ts
@@ -0,0 +1,85 @@
+interface Info {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+interface Dataparsed {
+    dt: Array<number>;
+    target: number;
+}
+
+const parseArguments2 = (args: Array<string>): Dataparsed => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 100) throw new Error('Too many arguments (100 maximum)');
+    let targ: number = 0;
+    if (!isNaN(Number(args[2]))) {
+        targ = Number(args[2]);
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+
+    let trainingVals: Array<number> = [];
+    for (var i=3; i<args.length; i++){
+        if(!isNaN(Number(args[i]))){
+            trainingVals.push(Number(args[i]));
+        }else{
+            throw new Error('Provided values were not numbers!');
+        }
+    }
+    return {
+        dt: trainingVals,
+        target: targ
+    }
+}
+
+const calculateExercises = (data: Array<number>, target: number): Info => {
+    const days: number = data.length;
+    let trainingD: number = 0;
+    let sum: number = 0;
+    data.forEach(function (item,index) {
+        if(item>0){
+            trainingD += 1;
+        }
+        sum += Number(item);
+    });
+    let averageTime: number = sum / days;
+    let rating: number;
+    let ratingDesc: string;
+    let succ: boolean = false;
+    if(averageTime>target){
+        rating = 3;
+        ratingDesc = "great you've reached your target!";
+        succ = true;
+    }else if(averageTime > (target/2)){
+        rating = 2;
+        ratingDesc = 'not too bad but could be better';
+    }else{
+        rating = 1;
+        ratingDesc = 'you must do better next week';
+    }
+    return {
+        periodLength: days,
+        trainingDays: trainingD,
+        success: succ,
+        rating: rating,
+        ratingDescription: ratingDesc,
+        target: target,
+        average: averageTime
+    }
+}
+
+try {
+    const { dt, target } = parseArguments2(process.argv);
+    console.log(calculateExercises(dt, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
